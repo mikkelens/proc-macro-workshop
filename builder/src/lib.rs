ee40409ae90d19ae50c8845375721fd7355d097b
Mikkel <Mikkel123sej@gmail.com>
2023-12-25 23:00:32 +0100

    some attempts

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,22 @@
-use proc_macro::TokenStream;
+use proc_macro::{Ident, Span, TokenStream};
+use syn::{parse::Parse, parse_macro_input, DeriveInput, __private::quote::quote};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+	let input = parse_macro_input!(input as DeriveInput);
+	let name = Ident::new(&format!("{}Builder", input.ident), Span::call_site());
 
-    unimplemented!()
+	let expanded = quote! {
+		pub struct #name {
+			#
+		}
+		impl #name {
+			pub fn builder() -> #name {
+				#name::new()
+			}
+		}
+	};
+
+	// unimplemented!()
+	TokenStream::new()
 }
